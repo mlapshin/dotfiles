156c3481aeae4dd0476bdbd8f5d94b575f040e9e
Mike Lapshin <sotakone@sotakone.com>
2012-10-10 01:41:09 +0400

    xinitrc, xsession and Xresources

--- mklinks.sh
+++ mklinks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 ln -sfvT /home/sotakone/dotfiles/.emacs.d /home/sotakone/.emacs.d
+
 ln -sfvT /home/sotakone/dotfiles/.vim /home/sotakone/.vim
 ln -sfvT /home/sotakone/dotfiles/.vimrc /home/sotakone/.vimrc
+
+ln -sfvT /home/sotakone/dotfiles/.Xresources /home/sotakone/.Xresources
+ln -sfvT /home/sotakone/dotfiles/.xinitrc /home/sotakone/.xinitrc
+ln -sfvT /home/sotakone/dotfiles/.xsession /home/sotakone/.xsession
