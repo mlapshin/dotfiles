27b79e764feeb45cad3f4c8a5ed2196153385352
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2012-12-11 15:27:56 +0400

    ruby indentation fix

--- .emacs.d/sotakone/ruby.el
+++ .emacs.d/sotakone/ruby.el
@@ -22,6 +22,28 @@
      (require 'ruby-tools)
      (require 'rspec-mode)
 
+     (defadvice ruby-indent-line (after line-up-args activate)
+       (let (indent prev-indent arg-indent)
+         (save-excursion
+           (back-to-indentation)
+           (when (zerop (car (syntax-ppss)))
+             (setq indent (current-column))
+             (skip-chars-backward " \t\n")
+             (when (eq ?, (char-before))
+               (ruby-backward-sexp)
+               (back-to-indentation)
+               (setq prev-indent (current-column))
+               (skip-syntax-forward "w_.")
+               (skip-chars-forward " ")
+               (setq arg-indent (current-column)))))
+         (when prev-indent
+           (let ((offset (- (current-column) indent)))
+             (cond ((< indent prev-indent)
+                    (indent-line-to prev-indent))
+                   ((= indent prev-indent)
+                    (indent-line-to arg-indent)))
+             (when (> offset 0) (forward-char offset))))))
+
      (idle-highlight-mode t)
      (rvm-use-default)
 
