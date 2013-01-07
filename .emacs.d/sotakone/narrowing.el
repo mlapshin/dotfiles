dbc774dc34fe3612e60ea28bd8fdee76ca014a05
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-01-07 12:03:58 +0400

    narrow-dwim on C-n C-n

--- .emacs.d/sotakone/narrowing.el
+++ .emacs.d/sotakone/narrowing.el
@@ -1,5 +1,17 @@
 (global-unset-key (kbd "C-n"))
 
-(global-set-key (kbd "C-n C-n") 'narrow-to-region)
+(defun narrow-dwim ()
+  "If buffer is not narrowed, narrow it to region or defun. If narrowed, widen it."
+  (interactive)
+  (if (and (eq (point-min) 1)
+           (eq (- (point-max) 1) (buffer-size)))
+      (if mark-active
+          (narrow-to-region)
+        (if (eq major-mode 'ruby-mode)
+            (narrow-to-ruby-def)
+          (narrow-to-defun)))
+        (widen)))
+
+(global-set-key (kbd "C-n C-n") 'narrow-dwim)
 (global-set-key (kbd "C-n C-d") 'narrow-to-defun)
 (global-set-key (kbd "C-n C-w") 'widen)
