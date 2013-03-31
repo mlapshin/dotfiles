64699df0456be64ecc5dc9693632b3868ec36130
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-03-31 16:48:40 +0400

    ace-jump fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,9 +15,3 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
-
-;; C-z should be rebinded at the end of initialization
-(require 'ace-jump-mode)
-
-(global-set-key (kbd "C-z") 'ace-jump-mode)
-(global-set-key (kbd "C-M-z") 'ace-jump-char-mode)
