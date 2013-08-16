9c233a2b6b8250d48429122a067cd1d602ce2805
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-08-16 12:20:45 +0400

    added melpa to elisp packages sources

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 (require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/vendor/helm")
