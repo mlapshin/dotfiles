1437ddd3d1647a5a01c8867a64526078fd1d7955
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-11-22 23:20:52 +0400

    clojure & nrepl mode auto-complete

--- .emacs.d/custom/clojure.el
+++ .emacs.d/custom/clojure.el
@@ -1,3 +1,5 @@
+(require 'ac-nrepl)
+
 (add-hook 'clojure-mode-hook
           (lambda ()
             (paredit-mode t)))
@@ -5,3 +7,9 @@
 (add-hook 'nrepl-mode-hook
           (lambda ()
             (paredit-mode t)))
+
+
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+
+
--- .emacs.d/sotakone-custom-variables.el
+++ .emacs.d/sotakone-custom-variables.el
@@ -11,9 +11,7 @@
  '(ac-expand-on-auto-complete nil)
  '(ac-ignore-case nil)
  '(ac-menu-height 30)
- '(ac-modes
-   (quote
-    (lua-mode haml-mode emacs-lisp-mode lisp-interaction-mode c-mode cc-mode c++-mode clojure-mode java-mode perl-mode cperl-mode python-mode ruby-mode ecmascript-mode espresso-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode)))
+ '(ac-modes (quote (lua-mode haml-mode emacs-lisp-mode lisp-interaction-mode c-mode cc-mode c++-mode clojure-mode java-mode perl-mode cperl-mode python-mode ruby-mode ecmascript-mode espresso-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode nrepl-mode)))
  '(ac-show-menu-immediately-on-auto-complete t)
  '(ac-stop-words (quote ("begin" "if" "else" "end" "do")))
  '(ac-trigger-key "TAB")
