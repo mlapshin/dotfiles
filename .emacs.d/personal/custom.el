a8a3be045685cccfc8e37724aef7b1f54e6d1206
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-07-02 11:38:43 +0400

    emacs improvements

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -3,7 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fci-rule-color "#383838"))
+ '(clojure-defun-style-default-indent nil)
+ '(fci-rule-color "#383838")
+ '(sql-postgres-login-params
+   (quote
+    ((user :default "mlapshin")
+     server
+     (database :default "mlapshin")
+     port)))
+ '(standard-indent 2)
+ '(tab-width 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/personal/smartparens.el
+++ .emacs.d/personal/smartparens.el
@@ -2,20 +2,18 @@
 
 (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
 (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
-(define-key sp-keymap (kbd "ESC <deletechar>") 'sp-unwrap-sexp) ;; M-delete
-
-(define-key sp-keymap (kbd "M-[ d") 'sp-forward-slurp-sexp)
-(define-key sp-keymap (kbd "M-[ c") 'sp-forward-barf-sexp)
 
 (define-key sp-keymap (kbd "C-M-l") 'sp-add-to-previous-sexp)
 (define-key sp-keymap (kbd "C-M-h") 'sp-add-to-next-sexp)
 (define-key sp-keymap (kbd "C-c j") 'sp-join-sexp)
-(define-key sp-keymap (kbd "C-c s") 'sp-split-sexp)
-(define-key sp-keymap (kbd "C-c d") 'sp-splice-sexp-killing-around)
 
 (define-key sp-keymap (kbd "C-M-a") 'sp-beginning-of-sexp)
 (define-key sp-keymap (kbd "C-M-e") 'sp-end-of-sexp)
 
 (define-key sp-keymap (kbd "M-{") (lambda ()
+                                    (interactive)
+                                    (sp-wrap-with-pair "{")))
+
+(define-key sp-keymap (kbd "M-[") (lambda ()
                                     (interactive)
                                     (sp-wrap-with-pair "[")))
