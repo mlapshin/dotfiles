26bbb9db8b81072da28fb9afc06ac3f3eb309f8f
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2017-07-26 00:37:34 +0300

    Update mklinks.sh

--- mklinks.sh
+++ mklinks.sh
@@ -5,28 +5,28 @@ ln -sfv ~/dotfiles/.emacs.d ~/.emacs.d
 ln -sfv ~/dotfiles/.vim ~/.vim
 ln -sfv ~/dotfiles/.vimrc ~/.vimrc
 
-ln -sfv ~/dotfiles/.Xresources ~/.Xresources
-ln -sfv ~/dotfiles/.xinitrc ~/.xinitrc
-ln -sfv ~/dotfiles/.xsession ~/.xsession
-ln -sfv ~/dotfiles/.xmodmaprc ~/.xmodmaprc
+# ln -sfv ~/dotfiles/.Xresources ~/.Xresources
+# ln -sfv ~/dotfiles/.xinitrc ~/.xinitrc
+# ln -sfv ~/dotfiles/.xsession ~/.xsession
+# ln -sfv ~/dotfiles/.xmodmaprc ~/.xmodmaprc
 
 ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sfv ~/dotfiles/.gemrc ~/.gemrc
+# ln -sfv ~/dotfiles/.gemrc ~/.gemrc
 
 ln -sfv ~/dotfiles/.bashrc ~/.bashrc
-ln -sfv ~/dotfiles/.config/awesome ~/.config/awesome
-ln -sfv ~/dotfiles/.config/twmn ~/.config/twmn
+# ln -sfv ~/dotfiles/.config/awesome ~/.config/awesome
+# ln -sfv ~/dotfiles/.config/twmn ~/.config/twmn
 
-ln -sfv ~/dotfiles/.urxvt ~/.urxvt
+# ln -sfv ~/dotfiles/.urxvt ~/.urxvt
 ln -sfv ~/dotfiles/.bash_profile ~/.bash_profile
 
 ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -sfv ~/dotfiles/bin ~/bin
 
-ln -sfv ~/dotfiles/.i3 ~/.i3
-ln -sfv ~/dotfiles/.conky ~/.conky
+# ln -sfv ~/dotfiles/.i3 ~/.i3
+# ln -sfv ~/dotfiles/.conky ~/.conky
 
-ln -sfv ~/dotfiles/.inputrc ~/.inputrc
-ln -sfv ~/dotfiles/.config/dunst ~/.config/dunst
-ln -sfv ~/dotfiles/.config/fontconfig ~/.config/fontconfig
+# ln -sfv ~/dotfiles/.inputrc ~/.inputrc
+# ln -sfv ~/dotfiles/.config/dunst ~/.config/dunst
+# ln -sfv ~/dotfiles/.config/fontconfig ~/.config/fontconfig
